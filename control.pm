62d8b260ba1602a9c2ce27f63282f038e5c3ba2e
Zdenek <zed448@icloud.com>
2015-12-05 19:56:31 +0100

    making module.pm instead pl script
    control.pm - is p5 module which exports function taking distribution
    name string, returns hash reference of dpkg/cydia control file values
    fetched throught MetaCPAN::Clien API
    
    use control;
    my $dpkg_control = get_control(‘p5-dist-name’) ;

--- control.pm
+++ control.pm
@@ -1,44 +1,44 @@
 #!/usr/bin/env perl
-use MetaCPAN::Client;
-use Data::Dumper;
 
-# note
-# add architecture (perl5/lib folder)A
-#
-my $m = MetaCPAN::Client->new();
-my $module = $m->module('PAR::Packer');
-my $release = $m->release('PAR-Packer');
-my $pod = $m->pod('PAR::Packer')->html;
+package control;
 
-##debug dumps
-#print $pm;
-#print $pod;
-#print Dumper $release;
-#print Dumper $module;
-my $d=$release->{data};
-my $meta=$d->{metadata};
+use strict;
+use warnings;
+use feature 'say';
+use JSON::Tiny qw(decode_json);
 
-my @deps=$d->{dependency};
-my $name_pm=$module->{data}{module};
-my $package=$meta->{name};
-my $version=$d->{version};
-my $description=$meta->{abstract};
-my $licence=$d->{licence};  # ???
-my $author=$d->{author};
-my $homepage='https://metacpan.org/pod/'.'';
 
-my $src_url= $d->{download_url};
+BEGIN {
+    require Exporter;
+    our $VERSION = 1.00;
+    our @ISA = qw(Exporter);
+    our @EXPORT = qw($control get_control);
+}
 
-print "package:  $package"."\n"."version:  $version"."\n"."description:  $description"."\n"."\n"."author:  $author"."\n";
-print "src url: $src_url"."\n";
-print "module name:  "; for(@$name_pm){ print $_->{name} }; print "\n";
-print "deps: "."\n";
+our $control = '';
+my $dist = '';
+my $meta_api_url = '';
+my $meta_json = '';
+my $meta = '';
 
-for $dep (@deps){
-    for $hash(@$dep){
-            if ($$hash{phase} eq 'runtime' and $$hash{relationship} eq 'requires'){
-                print "\t".$$hash{module}."\n";
-                #print $key."\-\>".$value."\n"; 
-            }
-    }
-}    
+my $control_conf = sub { 
+    my $c_json = open(my $fh, "<", "control.conf") or die "cannot open < control.conf: $!";
+    my $c_conf = decode_json $c_json;
+    return $c_conf;
+};
+
+my $get_control = sub {
+    $dist=shift;
+    $meta_api_url='https://api.metacpan.org/v0/release/'."$dist";
+    $meta_json=qx!curl -skL $meta_api_url!;
+    $meta=decode_json $meta_json;
+    return $meta;
+};
+
+sub get_control {
+    $dist = shift;
+    $control = $get_control->($dist);
+    return $control;
+}
+
+1;
