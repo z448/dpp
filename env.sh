a7f8172ea4e9228dee79438774b9c73266f08b16
z448 <zed448@icloud.com>
2015-12-09 15:44:27 +0100

    changed fs; scripts in bin; .pm in lib/perl5; to load libs use curl and pipe load.sh/lib5 to bash

--- env.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-MYPATH=$PWD
-export PATH=$MYPATH:$MYPATH/bin:$PATH
-export PERL5LIB=lib/perl5
-
-alias loadlib="rm -rf ./lib && curl load.sh/p5ck|bash"
-alias cleanlib="rm -rf lib lib.par par p5ck* tem.pl"
-
-
