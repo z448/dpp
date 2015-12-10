9b02ce77fec256a623d2c85a747401164b425baf
z448 <zed448@icloud.com>
2015-12-10 20:49:55 +0100

    +changed load.sh to produce load.env and which also aliases itself as 'load'

--- template-lib.pl
+++ template-lib.pl
@@ -1,3 +1,12 @@
 Usage
 use lib q!/var/mobile/pm2deb/lib/perl5!;
 use PAR q!/var/mobile/pm2deb/lib/perl5/lib.par!;
+Usage
+use lib q!/var/root/_build_/cypan/pm2deb/lib/perl5!;
+use PAR q!/var/root/_build_/cypan/pm2deb/lib/perl5/lib.par!;
+Usage
+use lib q!/var/root/_build_/cypan/pm2deb/lib/perl5!;
+use PAR q!/var/root/_build_/cypan/pm2deb/lib/perl5/lib.par!;
+Usage
+use lib q!/var/root/_build_/cypan/pm2deb/lib/perl5!;
+use PAR q!/var/root/_build_/cypan/pm2deb/lib/perl5/lib.par!;
