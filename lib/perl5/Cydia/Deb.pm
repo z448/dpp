72026bfb4ed8f2ce2d1047001f73fefddebd6296
z448 <zed448@icloud.com>
2015-12-17 03:13:13 +0100

    +Deb.pm: path renaming; +control: finished dependencies resolver

--- lib/perl5/Cydia/Deb.pm
+++ lib/perl5/Cydia/Deb.pm
@@ -3,18 +3,27 @@
 package Cydia::Deb;
 use Term::ANSIColor;
 use IO::All;
+use File::Copy;
 
-my (@files,$dir);
-unless(@ARGV){ $dir eq 'lib' } else { $dir eq $ARGV[0] }
+my (@files,$dir,$perl_ver);
+$perl_ver = '5.18.2';
 
-@files=grep{/packlist/}io->dir('lib')->All;
+#print $cwd;
+#unless($ARGV[0] =~ /.*/){ $dir = '~/cypan-build/usr/local/lib/perl5/lib' }
+unless($ARGV[0] =~ /.*/){ $dir = 'build/usr/local/lib/perl5/lib' }
+print $dir;
+@files=grep{/packlist/}io->dir('build/usr/local/lib/perl5')->All;
 
+my $b_dir_rx = qr/$dir/;
 for (@files){
     print "\n\n"; print colored(['bright_white'],$_)."\n";
     my @content=io($_)->slurp;
 
-    for (@content){
-        if(/\/usr\/local/){ next } else {
+      for (@content){
+        unless(/build/){ next } else {
         print $_;
-    }}
+        move("build/usr/local/lib/perl5/lib/perl5", "build/usr/local/lib/perl5/lib/".$perl_ver);
+        move("build/usr/local/lib/perl5/lib", "build/usr/local/lib/perl5/site_perl");
+        }
+    }
 }
--- stuff/notes.md
+++ stuff/notes.md
@@ -1,3 +1,18 @@
 #### debian manual
 https://www.debian.org/doc/debian-policy/index.html#contents
 
+
+
+use File::Copy;
+
+copy("file1","file2");
+copy("Copy.pm",\*STDOUT);'
+move("/dev1/fileA","/dev2/fileB");
+
+#find site_perl path
+perl -e 'for(@INC){ if(/\/Sys/){print}}';
+
+perl -e 'for(@INC){print $_."\n" if /Extras/}'
+
+
+
