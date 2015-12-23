daa0940667cbaca4e97702a4dc9c19750366ef5f
z448 <zed448@icloud.com>
2015-12-23 19:43:33 +0100

    +query() returns build details

--- lib/Cydia/Deb.pm
+++ lib/Cydia/Deb.pm
@@ -1,31 +1,43 @@
 #!/usr/bin/env perl
 
-package Cydia::Deb;
-use Term::ANSIColor;
-use IO::All;
-use File::Copy;
+ package Cydia::Deb;
+ use FindBin;
+ use lib "$FindBin::Bin/../lib";
+ use open qw< :encoding(UTF-8) >;
+ use Cydia::Control qw(get_control);;
+ use Term::ANSIColor;
+ use File::Copy;
+ use Filesys::Tree;
+ use File::Path;
+ use Getopt::Std;
 
-my (@files,$dir,$perl_ver);
-$perl_ver = '5.18.2';
+ getopts('p:m:i:', \%opts);
 
-#print $cwd;
-#unless($ARGV[0] =~ /.*/){ $dir = '~/cypan-build/usr/local/lib/perl5/lib' }
-$dir = 'build/usr/local/lib/perl5/lib';
-print $dir;
-@files=grep{/packlist/}io->dir('build/usr/local/lib/perl5')->All;
+ my $p5ver = sub { ...;
+     # find perl5 version
+ };
 
-my $b_dir_rx = qr/$dir/;
-for (@files){
-    print "\n\n"; print colored(['bright_white'],$_)."\n";
-    my @content=io($_)->slurp;
-
-      for (@content){
-        unless(/build/){ next } else {
-        print $_;
-        move("build/usr/local/lib/perl5/lib/perl5", "build/usr/local/lib/perl5/lib/".$perl_ver);
-        move("build/usr/local/lib/perl5/lib/".$perl_ver, "build/usr/local/lib/perl5/site_perl");
-        }
-    }
-}
+ my $build = sub {
+     $b{'dpkg_prefix'} = $opts{'p'};
+     $b{'dpkg_control'} = get_control($opts{'m'});
+     $b{'dpkg_name'} = $b{'dpkg_control'}{'name'};
+     $b{'path_dir'} = 'build/' . $b{'dpkg_name'} . '/usr/local/lib/perl5/lib';
+     $b{'path_debian'} = 'build/' . $b{'dpkg_name'} . '/DEBIAN';
+     return \%b;
+ };
 
 
+ my $query = sub {
+     my $b = &$build;
+     my $req = $opts{i};
+     if ( defined $opts{'i'} ){
+         print $$b{$req};
+     } else {
+         while (($key, $value) = each %$b) {
+             print $key.' -> '.$$b{$key}."\n";
+         }
+     }
+     print "\n\n";
+ };
+
+ &$query;
