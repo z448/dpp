380738946764c33a268ee1ca4baad08b2c72d16e
z448 <zed448@icloud.com>
2016-01-05 17:22:36 +0100

    +par archive; source dwl

--- lib/Cydia/Meta.pm
+++ lib/Cydia/Meta.pm
@@ -15,6 +15,9 @@ use Filesys::Tree;
 use File::Path qw(make_path remove_tree);
 use Data::Dumper;
 use Data::Printer;
+use Archive::Tar;
+use PAR::Dist;
+
 
 BEGIN {
     require Exporter;
@@ -29,11 +32,11 @@ my $control = sub {
     my $meta_url = 'http://api.metacpan.org/v0/module/'."$module".'?join=release';
     my $meta_json = qx!curl -sL $meta_url!;
     my $meta = decode_json $meta_json;
-     
+
     my $deps_line = sub {
         my $strings = shift;
         my @deps = ();
-        for( keys %$strings ){ push @deps, $_.', ' } 
+        for( keys %$strings ){ push @deps, $_.', ' }
         return \@deps;
     };
 
@@ -41,7 +44,7 @@ my $control = sub {
         Name         => $meta->{release}->{_source}->{name},
         Version      => $meta->{release}->{_source}->{version},
         Author       => $meta->{release}->{_source}->{author},
-        Description  => $meta->{release}->{_source}->{abstract}."\n".$meta->{description},  
+        Description  => $meta->{release}->{_source}->{abstract}."\n".$meta->{description},
         Homepage     => $metacpan.$meta->{module}[0]->{name},
         Depends      => $deps_line->($meta->{release}->{_source}->{metadata}->{prereqs}->{runtime}->{requires}),
         module_name  => $meta->{module}[0]->{name},
@@ -49,29 +52,49 @@ my $control = sub {
         source_url   => $meta->{release}->{_source}->{download_url},
         #pod         => $meta->{pod},
     };
+
     return $c;
+
 };
 
-        
+
     #
     # add & rw defaults from json here;
     #
-    
+
 
 my $paths = sub {
-    my ($module, $prefix) = @_; 
+    my ($module, $prefix) = @_;
     my $c = $control->( $module );
+
+    #my $dist = blib_to_par(); # make a PAR file using ./blib/
+    #install_par($dist);       # install it into the system
+    #uninstall_par($dist);     # uninstall it from the system
+    #sign_par($dist);          # sign it using Module::Signature
+    #verify_par($dist);        # verify it using Module::Signature
+
+    #install_par("http://foo.com/DBI-1.37-MSWin32-5.8.0.par"); # works too
+    #install_par("http://foo.com/DBI-1.37"); # auto-appends archname + perlver
+    #install_par("cpan://SMUELLER/PAR-Packer-0.975"); # uses CPAN author directory
 
     my $pkg = {
         prefix => $prefix,
         name => $prefix . lc $$c{Name},
         package => $$c{Name},
+        source_path => build_path => 'build/' . $$c{Name} . 'src',
         build_path => 'build/' . $$c{Name} . '/usr/local/lib/perl5/lib',
         control_path => 'build/' . $$c{Name} . '/DEBIAN/control',
         deb_name => $$c{Name} . 'deb',
     };
+
+    my $get_source_pkg = qx!curl -sLo $$pkg{$source_path} $$c{$source_url}!;
+
     return $pkg;
 };
+
+my $build = sub {
+    my ( $module ) = @_;
+    my ( $source_url, $source_path ) = ( $control->()
 
 sub meta_pm {
     my $c = $control->($ARGV[0]);
@@ -79,7 +102,7 @@ sub meta_pm {
     #for( keys %$c ){
     #    say $_.' -> '.$c->{ $_ };
     #}
-    
+
     #print "Depends: ";
     #print @{$$c{Depends}};
     #print "\n";
