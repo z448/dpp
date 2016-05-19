46d32f6306b9db69eff7258b27a3ba0704dd1c22
z448 <zed448@icloud.com>
2016-05-20 00:22:17 +0200

    -

--- lib/Cydia/Meta.pm
+++ lib/Cydia/Meta.pm
@@ -161,7 +161,7 @@ my $web = sub {
         #}
 
     # add current pm div into @body
-    @body = decode_json $index->{body};
+    push(@body, @{$index->{body}});
     push @body, $m->{ div };
     #uniq 
     my %body_seen = ( );
