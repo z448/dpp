dc3ab8753b561ae143970f66edc408ba5f3fa12b
z448 <zed448@icloud.com>
2016-05-20 08:42:33 +0200

    -

--- lib/Cydia/Meta.pm
+++ lib/Cydia/Meta.pm
@@ -153,26 +153,22 @@ my $web = sub {
     my ($index, @pipe, @body) = ();
 
     # load header/body/footer
-    #{
-        open(my $fh,"<","$ENV{DPP}/assets/html/index.json") || die "$ENV{DPP}/assets/html/index.json $!";
-        $index = <$fh>;
-        $index = decode_json $index;
-        close $fh;
-        #}
+    open(my $fh,"<","$ENV{DPP}/assets/html/index.json") || die "$ENV{DPP}/assets/html/index.json $!";
+    $index = <$fh>;
+    $index = decode_json $index;
+    close $fh;
 
     # add current pm div into @body
-    push(@body, @{$index->{body}});
+    push(@body, @{$index{body}});
     push @body, $m->{ div };
     #uniq 
     my %body_seen = ( );
     @body = grep { ! $body_seen{$_} ++ } @body;
     $index->{ body } = encode_json \@body;
 
-    #{
-        open($fh,">","$ENV{DPP}/assets/html/index.json") || die "cant open index.json: $!";
-        print $fh encode_json $index;
-        close $fh;
-        #}
+    open($fh,">","$ENV{DPP}/assets/html/index.json") || die "cant open index.json: $!";
+    print $fh encode_json $index;
+    close $fh;
 
     # append current module div to div.html
     #{
