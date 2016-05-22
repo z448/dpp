2844cddba74faf24fe260de1fd817577f800f841
z448 <zed448@icloud.com>
2016-05-22 21:56:29 +0200

    bin/tools/index.pl

--- bin/tools/index.pl
+++ bin/tools/index.pl
@@ -9,11 +9,11 @@ use Data::Dumper;
 use JSON;
 use open qw<:encoding(UTF-8)>;
 
-my $head_file = './head.html';
-my $foot_file = './foot.html';
-my $body_file = './body.html';
-my $css_file = './style.css';
-my @tag_file = ( $head_file, $foot_file, $body_file, $css_file );
+my $option = $ARGV[0] || print "\nUsage:\n\t - create index.json from html files \n\t\t'index.pl -j file1 file2 file#'" .
+                                            "\n\n\t - create index.html from index.json (or $2) file \n\t\t'index.pl -h'\n\n";
+
+my @tag_file = @ARGV;
+shift @tag_file;
 
 my $make_index = sub {
     my $tag_file = shift;
@@ -21,8 +21,8 @@ my $make_index = sub {
 
     for(@$tag_file){
         my $tag = $_;
-        $tag =~ s/(\.\/)(.*?)(\..*)/$2/;
-        print $tag . "\n";
+        $tag =~ s/(.*?)(\..*)/$1/;
+        $tag = $1;
 
         open(my $fh,"<",$_) || die "cant open $_: $!";
         $index->{$tag} = [];
@@ -42,13 +42,14 @@ my $write_index = sub {
 
     if($file_type eq 'json'){
         $index = $make_index->(\@tag_file);
-        open(my $fh,">",$file) || "cant open $file";
-        print $fh $index;
-        close $fh;
+        #open(my $fh,">",$file) || die "cant open $file";
+        #print $fh $index;
+        print $index;
+        #close $fh;
     }
     elsif( $file_type eq 'html' ){
-        $file = 'index.json';
-        open(my $fh,"<",$file) || "cant open $file";
+        $file = $ARGV[1] || 'index.json';
+        open(my $fh,"<",$file) || die "cant open $file";
         $index = <$fh>;
         $index = decode_json $index;
 
@@ -56,24 +57,14 @@ my $write_index = sub {
         for( @{$index->{style}} ){ print $_ }
         for( @{$index->{body}} ){ print $_ }
         for( @{$index->{foot}} ){ print $_ }
-
-        # for(keys %$index){
-        #    for my $line ( @{$index->{$_}} ){
-        #            print $line;
-        #    }
-        #}
     }
 };
 
-#$write_index->('json');
-$write_index->('html');
 
-__DATA__
-        $file = 'index.json';
-        open(my $fh,"<",$file) || die "cant open $file: $!";
-        $index = <$fh>:
-        $index = decode_json $index;
-        $body = $m->{div};
+sub start {
+    my $mode = shift;
+    if($mode eq '-j'){ $write_index->('json') }
+    if($mode eq '-h'){ $write_index->('html') }
+}
 
-};
-
+start($option);
