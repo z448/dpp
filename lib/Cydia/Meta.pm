0ad99bae867652fadc884544cc6d5ca8023def89
z448 <zed448@icloud.com>
2016-05-23 02:39:36 +0200

    index.html todo: write into index.html, uniqe, icon background

--- lib/Cydia/Meta.pm
+++ lib/Cydia/Meta.pm
@@ -150,7 +150,8 @@ my $web = sub {
     my $pm = shift;
     print '### $web: $pm is ' . $pm;
     my $m = $meta->( $pm );
-    my ($index, @pipe, @body) = ();
+    my ( @pipe, @body ) = ();
+    my $index = {};
 
     # load header/body/footer
     open(my $fh,"<","$ENV{DPP}/assets/html/index.json") || die "$ENV{DPP}/assets/html/index.json $!";
@@ -162,14 +163,15 @@ my $web = sub {
 
     # add current pm div into @body
     #push(@{$index{body}}, @body);
-    push @body, $m->{ div };
+    push $index->{body}, @{$m->{ div }};
     #uniq 
-    my %body_seen = ( );
-    @body = grep { ! $body_seen{$_} ++ } @body;
+    #my %body_seen = ( );
+    #@body = grep { ! $body_seen{$_} ++ } @body;
+    #@{$index->{body}} = grep { ! $body_seen{$_} ++ } @{$index->{body}};
     #$index{body} = encode_json \@body;
 
     open($fh,">","$ENV{DPP}/assets/html/index.json") || die "cant open index.json: $!";
-    #print $fh encode_json \%index;
+    print $fh encode_json $index;
     close $fh;
 
     # append current module div to div.html
