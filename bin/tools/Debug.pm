6197d8ecc7861a0bfc9db136e5037d4570e1c5da
z448 <zed448@icloud.com>
2016-05-31 09:34:44 +0200

    debug: Meta.pm json read control

---
+++ bin/tools/Debug.pm
@@ -0,0 +1,46 @@
+package Debug;
+
+use 5.010;
+use warnings;
+use strict;
+
+
+use JSON;
+use File::Copy;
+use File::Path;
+use Encode;
+use Data::Dumper;
+use Config;
+use Config::Extensions qw( %Extensions );
+use Cwd qw< abs_path >;
+use open qw<:encoding(UTF-8)>;
+
+BEGIN {
+    require Exporter;
+    our $VERSION = 0.01;
+    our @ISA = qw(Exporter);
+    our @EXPORT_OK = qw( control_json meta web init);
+}
+
+
+my $control = {
+    Name         => '$m->{distribution}',
+    Version      => '$m->{version}',
+    Author       => '$m->{author}',
+    Section      => 'Perl',
+    Description  => '$m->{abstract}',
+    Maintainer   => 'name (nick) <email>',
+    Homepage     => '$metacpan . $meta_p->{module}[0]->{name}',
+    Architecture => '$arch->()',
+    Package      => '$prefix . lc $m->{distribution} . "-p5"',
+    Depends      => '$deps->($module)',
+};
+
+sub control_json {
+    my @control = ();
+    push @control, $control;
+    #return $control_p5;
+
+    my $control_json = encode_json \@control;
+    return $control_json;
+}
