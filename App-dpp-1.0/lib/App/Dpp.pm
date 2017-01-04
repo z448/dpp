3d9be035ce6b4f9fc8ceab515d8aabdfc00a2459
Zdenek <z@nuc>
2017-01-04 12:30:43 +0100

    cleanup

--- App-dpp-1.0/lib/App/Dpp.pm
+++ App-dpp-1.0/lib/App/Dpp.pm
@@ -26,30 +26,30 @@ BEGIN {
     require Exporter;
     our $VERSION = 0.01;
     our @ISA = qw(Exporter);
-    our @EXPORT_OK = qw( control meta web init cleanup );
+    our @EXPORT_OK = qw( control meta web init );
 }
 
 our $VERSION = '1.0';
 
 
-my $dpp = "$ENV{HOME}/dpp";
-my $dir = {
-    dpp             =>  $dpp,
-    assets          =>  $dpp,
+#my $dpp = "$env{home}/dpp";
+#my $dir = {
+#    dpp             =>  $dpp,
+#    assets          =>  $dpp,
     #assets          =>  $dpp . '/' . 'assets',
     #assets_html     =>  $dpp . '/' . 'assets' . '/' . 'html',
-    build           =>  $dpp . '/' . 'build',
-    stash           =>  $dpp . '/' . 'stash',
-    deb             =>  $dpp . '/stash/deb',
-};
+#    build           =>  $dpp . '/' . 'build',
+#    stash           =>  $dpp . '/' . 'stash',
+#    deb             =>  $dpp . '/stash/deb',
+#};
 
 
-my $cleanup = sub {
-    my $dirty_dir = shift;
-    system("chmod -R 0755 $dirty_dir");
-    system("rm -r $dirty_dir");
-    #rmdir $dirty_dir;
-};
+#my $cleanup = sub {
+    #   my $dirty_dir = shift;
+    #system("chmod -R 0755 $dirty_dir");
+    #system("rm -r $dirty_dir");
+    ##rmdir $dirty_dir;
+#};
 
 
 my $meta_api = sub {
@@ -74,64 +74,57 @@ my $perl_version = sub {
 ### init dpp direcories
 my $init = sub {
     my $get = shift;
+    my $dpp_home = ref $get eq 'HASH' ? $get->{dpp} : "$ENV{HOME}/dpp";
+
+    my $dir = {
+        dpp             =>  $dpp_home,
+        deb             =>  $dpp_home . '/' . 'deb',
+    };
     
-    # return dir PATH if param else create dirs and return \%dir
-    if($get){
-        return $dir->{$get};
+    # return dir PATH if param is string else create dirs and return \%dir
+    # if its a ref it passing getopts() (currently custom home for dpp)
+    unless(ref $get){
+        return $dir->{$get} if defined $dir->{$get};
     } else {
-        mkpath $dpp;
-        chmod( 0755, $dpp);
+        mkpath $dpp_home;
+        chmod( 0755, $dpp_home);
         for(keys %$dir){ mkpath $dir->{$_} }
-        
-        my %config = (
-            architecture    =>  'all',
-            package_prefix  =>  'lib',
-            section         =>  'perl',
-            maintainer      =>  'Your Name <your@email.com>',
-        );
 
-        open(my $fh,'<', "$ENV{HOME}/.gitconfig");
-        my($name, $email, $user) = ();
-        while(<$fh>){
-            chomp $_;
-            if(/\[.*\]/){ $user = 0 }
-            if(/\[user\]/){ $user = 1 }
-            if($user){
-                if(/name = (.*)/){ $name = $1 }
-                if(/email = (.*)/){ $email = $1 }
+        unless(-f "$ENV{HOME}/dpp.conf"){
+            my %config = (
+                architecture    =>  'all',
+                package_prefix  =>  'lib',
+                section         =>  'perl',
+                maintainer      =>  'Your Name <your@email.com>',
+            );
+            if(-f "$ENV{HOME}/.gitconfig"){
+                open(my $fh,'<', "$ENV{HOME}/.gitconfig");
+                my($name, $email, $user) = ();
+                while(<$fh>){
+                    chomp $_;
+                    if(/\[.*\]/){ $user = 0 }
+                    if(/\[user\]/){ $user = 1 }
+                    if($user){
+                        if(/name = (.*)/){ $name = $1 }
+                        if(/email = (.*)/){ $email = $1 }
+                    }
+                }; 
+                close $fh;
+                $config{maintainer} = "$name <$email>";
             }
-        }; 
-        close $fh;
-        $config{maintainer} = "$name <$email>";
 
-        open($fh,'>', "$ENV{HOME}/dpp/dpp.conf") || die "cant write to $ENV{HOME}/dpp/dpp.conf: $!";
-        for(keys %config){ print $fh "$_=$config{$_}\n" }
-        close $fh;
+            open(my $fh,'>', "$ENV{HOME}/dpp.conf");
+            for(keys %config){ print $fh "$_=$config{$_}\n" }
+            close $fh;
+        }
 
-        my $index_data = <DATA>;
-        my $index_file = $dir->{dpp} . '/' . 'index.json';
-        open($fh,'>',$index_file) || die "cant open $index_file: $!";
-        print $fh $index_data;
-        close $fh;
-        #symlink ~/.dpp/assets/index.json --> index.json
-        #my $index_link = init('assets') . "/index.json";
-
-        my $control_file = $dir->{dpp} . '/' . 'control.json';
-        #symlink ~/.dpp/assets/control.json --> control.json
-        #my $control_link = init('assets') . "/control.json";
-
-        #my $dpp_install_dir = abs_path($0);
-        #$dpp_install_dir =~ s/(.*)\/bin\/dpp$/$1/;
-        #$dpp_install_dir = $dpp_install_dir . '/' . 'lib/perl5/site_perl/' . $perl_version->() . '/App/Dpp';      
-
-        #my $index_file = $dpp_install_dir . '/' . 'index.json';
-        #chmod( 0644, $index_file);
-        #symlink $index_file, $index_link;
-
-        #my $control_file = $dpp_install_dir . '/' . 'control.json';
-        #chmod( 0644, $control_file);
-        #symlink $control_file, $control_link;
-
+        unless( -f "$dir->{dpp}/index.json" ){
+            my $index_data = <DATA>;
+            my $index_file = $dir->{dpp} . '/' . 'index.json';
+            #my $control_file = $dir->{dpp} . '/' . 'control.json';
+            open(my $fh,'>',$index_file) || die "cant open $index_file: $!";
+            print $fh $index_data; close $fh;
+        }
         return $dir;
     }
 };
@@ -232,7 +225,8 @@ my $meta = sub {
     my $m = $meta->{release}->{_source};
     my $stratopan = $graph.$m->{name};
     my $prefix = 'lib';
-    my $assets = $dir->{'assets'};
+    my $assets = $init->{dpp};
+    #my $assets = $dir->{'assets'};
     my $deb_url = "http://load.sh/cydia/deb/" . $prefix . lc $m->{distribution} . '-p5220' . '.deb';
 
     ### if arch is iPhone use 'iphoneos-arm' string in control file otherwise use 'all'
@@ -291,7 +285,8 @@ my $meta = sub {
 
 my $web = sub {
     my $pm = shift;
-    my $index_json = $dir->{'assets'} . '/' . 'index.json';
+    my $index_json = $init->{dpp} . '/' . 'index.json';
+    #my $index_json = $dir->{'assets'} . '/' . 'index.json';
     my $m = $meta->( $pm );
     my ( @pipe, @body ) = ();
     my $index = {};
@@ -354,11 +349,11 @@ sub init {
     my $init_status = $init->($get);
 }
 
-sub cleanup {
-    my $dirty_dir = shift;
-    #print '$cleanup->(' . $dirty_dir . ')';
-    $cleanup->($dirty_dir);
-};
+#sub cleanup {
+#   my $dirty_dir = shift;
+#   #print '$cleanup->(' . $dirty_dir . ')';
+#   $cleanup->($dirty_dir);
+#};
 
 __DATA__
 {"body":["<div class=\"dpp\"> </div>\n","<div class=\"dpp\"><a href=\"deb/.stash/deb/libhttp-tiny-p5.deb\" target=\"_blank\"><i class=\"fa fa-download\" aria-hidden=\"true\"></i> &nbsp;</a>","<a href=\"https://widgets.stratopan.com/wheel?q=HTTP-Tiny-0.058\" target=\"_blank\"><i class=\"fa fa-asterisk\" aria-hidden=\"true\"></i>&nbsp;</a><a href=\"http://api.metacpan.org/v0/pod/HTTP::Tiny?content-type=text/plain\" target=\"_blank\"><i class=\"fa fa-file\" aria-hidden=\"true\"></i></a></div>","<div class=\"module\">HTTP::Tiny</div>","<div class=\"description\">A small, simple, correct HTTP/1.1 client</br></div>"],"foot":["<div class=\"footer\" align=\"center\" >dpp<br></div>\n","</body>\n","</html>\n"],"head":["<!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.0 Transitional//EN\" \"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd\">\n","<html xmlns=\"http://www.w3.org/1999/xhtml\">\n","<head>\n","<title>dpp</title>\n","<meta http-equiv=\"Content-Type\" content=\"text/html; charset=utf-8\" />\n","<meta name=\"viewport\" content=\"width=device-width; initial-scale=1.0; maximum-scale=1.0; user-scalable=0;\"/>\n","<meta name=\"apple-mobile-web-app-capable\" content=\"yes\" />\n","<meta name=\"apple-mobile-web-app-status-bar-style\" content=\"black-translucent\"/>\n","<script src=\"https://code.jquery.com/jquery-2.2.3.min.js\" integrity=\"sha256-a23g1Nt4dtEYOj7bR+vTu7+T8VP13humZFBJNIYoEJo=\" crossorigin=\"anonymous\"></script>\n","<link rel=\"stylesheet\" href=\"https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.6.3/css/font-awesome.min.css\" />\n","<link rel=\"stylesheet\" href=\"//code.cdn.mozilla.net/fonts/fira.css\">\n","<link rel=\"stylesheet\" href=\"//netdna.bootstrapcdn.com/bootswatch/3.3.0/paper/bootstrap.min.css\" />\n","<link rel=\"stylesheet\" type=\"text/css\" href=\"style.css\">\n","</head>\n","\n","<body bgcolor=\"#090311\">\n","<div align=\"center\" >\n","<div class=\"headtext\"><br><br><sub>This is APT repository index.</sub><br><br></div>\n"],"style":["<style type=\"text/css\"> \n","\n",".slideshow-overlay {\n","    display: block;\n","    position: fixed;\n","    left: 0;\n","    top: 0;\n","    overflow: hidden;\n","    z-index: -99;\n","    height: 100%;\n","    width: 100%;\n","}\n","\n",".fa-download {\n","    background: #090311;\n","}\n","\n",".dpp {\n","    background: #090311;\n","}\n","\n",".headtext {\n","\tfont-family: 'Fira Mono';\n","\tfont-size: 11px;\n","    text-align = \"center\";\n","\tbackground : #090311;\n","    color: #8E8E8E;\n","    top: 2px;\n","    left: 0;\n","    right: 0;\n","}\n","\n",".description {\n","\tfont-family: 'Fira Mono';\n","\tfont-size: 11px;\n","    text-align = \"center\";\n","\tbackground : #090311;\n","    color: #4F4F50;\n","}\n","\n",".code{\n","    font-size: 10px;\n","    bottom: 5px;\n","\n","}\n","\n",".module {\n","    font-family: 'Open Sans', sans-serif;\n","    text-align = \"center\";\n","\tfont-size: 12px;\n","\tbackground : #090311;\n","\tcolor: #fefefe;\n","}\n","\n",".header{\n","\tfont-family: 'Fira Mono';\n","\tfont-size: 0;\n","\tcolor: white;\n","    -webkit-overflow-scrolling: touch;\n","    text-align: center;\n","    background: black;\n","    position: fixed;\n","    left: 0;\n","    right: 0;\n","    top: 0;\n","    height: 10px;\n","}    \n",".footer {\n","    font-family: 'Open Sans', sans-serif;\n","    font-weight: 600;\n","\tfont-size: 8px;\n","    border: 0;\n","    -webkit-overflow-scrolling: touch;\n","\tcolor: #4F4F50;\n","    text-align: center;\n","\tbackground : #1e1d20;\n","    position: fixed;\n","    left: 0;\n","    right: 0;\n","    bottom: 0;\n","    height: 13px;\n","}    \n","</style> \n"]}
