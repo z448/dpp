dea6ce121890260fddabefc8740415c9ddf4c01b
Zdenek <z@nuc>
2017-01-15 21:57:53 +0100

    -

---
+++ App-dpp-1.0/env.sh
@@ -0,0 +1,11 @@
+
+# for i in $(seq 1 1000);do clear && ls /home/zdenek/dpp/deb && sleep 1;done
+
+# rm -rf /home/zdenek/dpp/deb/tmp/* && mv ../libmoose-perl-nuc.deb . && ar -x lib* && tar -xvf control.tar.gz && cat preinst
+
+# top -p $(pidof program | tr ' ' ',')
+
+# perl -MMoose -e 'print $Moose::VERSION ."\n";'
+
+bind '"\C-p":"cd /home/zdenek/dpp/deb && pwd\n"';
+bind '"\C-g":"cd /home/zdenek/Documents/dpp/App-dpp-1.0 && pwd;\n"';
